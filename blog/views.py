6b328d82d4bc7aedb2394f96a86dfc4e1bb50b1a
sergo coraza <scorazago@gmail.con>
2019-08-28 20:04:12 -0500

    update and delete post

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
-
-post = [
-    {
-        'author':'sergioCG',
-        'title':'Blog1',
-        'content':'Primer post',
-        'date':'Junio 27'
-    },
-    {
-        'author':'sergioCG1',
-        'title':'Blog2',
-        'content':'Primer post 1',
-        'date':'Junio 27'
-    },
-]
 
 def home(request):
     context = {
-        'post': Post.objects.all()
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    field = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    field = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post =  self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
      return render(request,'blog/about.html',{'title':'about'})
